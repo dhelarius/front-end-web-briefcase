17da8ed0fc78e66d52b1c02acce8d289583bb074
Dario Jimenez Ayala <djimenez@swisherdominicana.com>
2021-02-25 17:59:51 -0400

    refactoring design

--- faq-accordion-card-main/js/script.js
+++ faq-accordion-card-main/js/script.js
@@ -4,12 +4,16 @@ let acc = document.getElementsByClassName('accordion');
 for (let i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    let panel = this.nextElementSibling;
+    let arrow = this.nextElementSibling;
+    let panel = document.getElementsByClassName("panel")[i];
     if (panel.style.display === "block") {
       panel.style.display = "none";
+      arrow.style.transform = "rotate(0deg)";
+      this.style.fontWeight = "normal";
     } else {
       panel.style.display = "block";
+      arrow.style.transform = "rotate(-180deg)";
+      this.style.fontWeight = "bold";
     }
   });
-  console.log(i);
 }
